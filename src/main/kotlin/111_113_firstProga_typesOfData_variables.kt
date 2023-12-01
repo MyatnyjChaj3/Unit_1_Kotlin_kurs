a41aeafc828b00d633164947b49698d86b61be1d
MyatnyjChaj3 <anastasiaisenko25@gmail.com>
2023-12-01 09:48:42 +0300

    first proga!

---
+++ src/main/kotlin/111_113_firstProga_typesOfData_variables.kt
@@ -0,0 +1,8 @@
+// способы быстрого написания fun main() {}:
+// main + tab -> fun main() {}
+// maina + tab -> fun main(args: Array<String>) {}
+
+fun main() {
+    println("Hello, World!") // print/println - вывод
+                             // в конце не нужно никаких знаков вроде ";"
+}
